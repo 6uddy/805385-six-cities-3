87d5b0ef7d74138cffb9c4cc8c74bf4cb60b36f3
Pavel Vlasov <pavelvlasovwork@gmail.com>
2020-03-08 21:52:21 +0300

    module4-task2 Правки

--- src/components/reviews-list/reviews-list.jsx
+++ src/components/reviews-list/reviews-list.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import nanoid from 'nanoid';
 import {ReviewItem} from '../review-item/review-item.jsx';
 
 export const ReviewsList = ({reviews}) => (
